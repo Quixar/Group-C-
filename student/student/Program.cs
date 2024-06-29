3f30311df0988d2a4185fac277e3b66f1f5c4fbb
Quixar <145463262+Quixar@users.noreply.github.com>
2024-06-29 17:51:10 +0300

    Add files via upload

---
+++ student/student/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace student;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Group g = new Group();
+
+        g.AddStudent(s);
+        g.AddStudent(s2);
+        g.AddStudent(s3);
+
+        s.AddExam(12);
+        s2.AddExam(6);
+        s3.AddExam(7);
+
+        g.ExpelBadStudent();
+        g.ShowGroup();
+        Group copy = new Group();
+        
+        g.ShowGroup();
+        g.TransferStudent(s, copy);
+        g.ShowGroup();
+        copy.ShowGroup();
+    }
+}
+
